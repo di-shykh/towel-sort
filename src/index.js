4b88b39d1d4c6aedd232425c29e01d57ae145a61
di-shykh <di49@mail.ru>
2024-09-03 16:50:28 +0300

    feat: add solution of task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  function compareNum(a,b) {
+    return a-b;
+  }
+  function compareNumInDifOder(a,b) {
+    return b-a;
+  }
+  let tempArr=[];
+  if (!matrix) return [];
+  for (let i=0; i<matrix.length; i++) {
+    if (i%2) {
+      matrix[i].sort(compareNumInDifOder);
+    }
+    else matrix[i].sort(compareNum);
+    matrix[i].forEach(element => {
+      tempArr.push(element);
+    });
+  }
+
+  return tempArr;
 }
